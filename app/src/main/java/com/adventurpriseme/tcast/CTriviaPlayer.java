b26a4e7dd36a88c571216fdc525f63d3d499cba0
tbremm <juggling.engineer@gmail.com>
2014-11-24 20:27:30 -0500

    Initial commit

---
+++ app/src/main/java/com/adventurpriseme/tcast/CTriviaPlayer.java
@@ -0,0 +1,93 @@
+package com.adventurpriseme.tcast;
+
+/**
+ * This class defines a player for the trivia game.
+ * <p/>
+ * Created by Timothy on 11/19/2014.
+ * Copyright 11/19/2014 adventurpriseme.com
+ */
+public class CTriviaPlayer implements IPerson
+	{
+	// Data members
+	private String  m_strName   = "";
+	private boolean m_bWillHost = true;
+	private int     m_nScore    = 0;
+
+	/**
+	 * Constructor
+	 */
+	public CTriviaPlayer ()
+		{
+		}
+
+	/**
+	 * Get the person's name.
+	 *
+	 * @return a string containing the person's name.
+	 */
+	@Override
+	public String getName ()
+		{
+		return m_strName;
+		}
+
+	/**
+	 * Set the person's name.
+	 *
+	 * @param strName
+	 * 		(required)  The new player name
+	 */
+	@Override
+	public void setName (String strName)
+		{
+		m_strName = strName;
+		}
+
+	/**
+	 * Get the player's willingness to host a game.
+	 *
+	 * @return True if player is willing to host, false otherwise.
+	 */
+	public boolean getWillHost ()
+		{
+		return m_bWillHost;
+		}
+
+	/**
+	 * Set the player's willingness to host a game.
+	 *
+	 * @param bWillHost
+	 * 		(required)  True if player is willing to host, false otherwise.
+	 */
+	public void setWillHost (boolean bWillHost)
+		{
+		if (m_bWillHost != bWillHost)
+			{
+			m_bWillHost = bWillHost;
+			}
+		}
+
+	/**
+	 * Gets the player's score.
+	 *
+	 * @return The player's score.
+	 */
+	public int getScore ()
+		{
+		return m_nScore;
+		}
+
+	/**
+	 * Sets the player's score.
+	 *
+	 * @param nScore
+	 * 		(optional) Sets the player's score.
+	 */
+	public void setScore (int nScore)
+		{
+		if (m_nScore != nScore)
+			{
+			m_nScore = nScore;
+			}
+		}
+	}
---
+++ app/src/main/java/com/adventurpriseme/tcast/MainActivity.java
@@ -0,0 +1,57 @@
+package com.adventurpriseme.tcast;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends Activity
+	{
+
+	@Override
+	protected void onCreate (Bundle savedInstanceState)
+		{
+		super.onCreate (savedInstanceState);
+		setContentView (R.layout.activity_main);
+
+		if (getActionBar () != null)
+			{
+			getActionBar ().setDisplayHomeAsUpEnabled (true);
+			}
+		}
+
+	@Override
+	public boolean onCreateOptionsMenu (Menu menu)
+		{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater ().inflate (R.menu.menu_main, menu);
+		return true;
+		}
+
+	@Override
+	public boolean onOptionsItemSelected (MenuItem item)
+		{
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId ();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings)
+			{
+			return true;
+			}
+
+		return super.onOptionsItemSelected (item);
+		}
+
+	// onClick handler for the playgame button
+	// This launches the trivia game entry intent
+	public void onPlayGame (View view)
+		{
+		Intent intent = new Intent (this, PlayTriviaActivity.class);
+		startActivity (intent);
+		}
+	}
