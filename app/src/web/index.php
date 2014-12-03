8941203f16657b1bf124101d44078690a031976f
Gabe <gnoblesmith@gmail.com>
2014-12-02 19:46:51 -0500

    reorganizing webcode for transition to adventurpriseme

---
+++ app/src/web/index.php
@@ -0,0 +1,36 @@
+<title> TriviaCast! </title>
+
+	<head>
+		<link rel="stylesheet" type="text/css" href="TriviaCast.css" media="screen" />
+	<!--
+	<script src="TriviaCast.js"> </script>
+	-->
+		<script src="jquery-1.11.1.js"></script>
+		<script language="javascript" type="text/javascript" src="Trivia.js"></script>
+		<script language="javascript" type="text/javascript" src="Cast.js"></script>
+		<script type="text/javascript" src="//www.gstatic.com/cast/sdk/libs/receiver/2.0.0/cast_receiver.js"></script>
+		<script type="text/javascript" src="Cast.js"></script><script type="text/javascript">
+
+	</head>
+	<!--
+	<body onload="init()">
+		<div align="center">
+		<canvas id="canvas" width="500" height="600" style="background-color: #000000">
+			HTML5 canvas not supported. shit.
+		</canvas>
+		</div>
+	</body>
+	-->
+	<body>
+		<div id="container">
+			<div id="qbox">
+				<div id="qboxtext">
+				</div>
+			<div id="abox">
+				<div id="aboxtext">
+				</div>
+			</div>
+		</div>
+		<div id="timer">100</div>
+	</body>	
+	
