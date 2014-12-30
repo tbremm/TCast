4c90a9ded3463a88a353609cf5d82bca8adbf7cc
tbremm <juggling.engineer@gmail.com>
2014-12-30 12:18:23 -0500

    Added GetTrueFalseQuestion.php to the repo, because it's live on the website as of this commit.

---
+++ app/src/web/GetTrueFalseQuestion.php
@@ -0,0 +1,43 @@
+<?php
+	$servername = "localhost";
+	$username = "trivia1";
+	$password = "trivia2";
+	$dbname = "dbTrivia";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+
+	//$sql = "SELECT * FROM `Basic Trivia` ORDER BY RAND() LIMIT 1";
+	$sql = "SELECT * FROM `TABLE 2` ORDER BY RAND() LIMIT 2";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+	
+		//$row1 = $result->fetch_assoc();
+        //$row2 = $result->fetch_assoc();
+
+		$row1 = mysqli_fetch_assoc($result);
+		$row2 = mysqli_fetch_assoc($result);
+		
+        $doTrue = rand(0,1);
+
+		echo $row1["COL 2"].":";
+		if ($doTrue) {
+		    echo $row1["COL 3"].":";
+	    } else {
+	        echo $row2["COL 3"].":";
+	    }
+	    echo $doTrue;
+		
+		mysqli_free_result($result);
+	} else {
+		//echo "0 results";
+	}
+	$conn->close();
+?>
+	
